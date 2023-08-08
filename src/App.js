584e8f7f9c4ab6619c6cbf7e512837a64c9e13ec
“Hem2021” <hemantpanchariya2073@gmail.com>
2023-08-09 02:05:42 +0530

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.scss';
+// import Navbar from './components/Navbar/Navbar.jsx';
+import Navbar from './Components/Navbar/Navbar.jsx';
+import Header from './container/Header/Header.jsx';
+import About from './container/About/About.jsx';
+import Globe from './container/Globe/Globe.jsx';
+import Explore from './container/Explore/Explore.jsx';
+import Blogs from './container/Blogs/Blogs.jsx';
+import Footer from './container/Footer/Footer.js';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <About />
+      <Globe />
+      <Explore />
+      {/* <Blogs /> */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
